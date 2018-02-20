2112b6f9086a00c636d750fe40166b43e7be9d02
artes07 <artes07333@gmail.com>
2018-02-20 16:02:58 +0200

    add get from server db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CarsService} from './cars.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   cars = [];
+
+  constructor(private carsServices: CarsService) {}
+
+  loadCars() {
+    this.carsServices
+      .getCars()
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
+import {CarsService} from './cars.service';
 
 
 @NgModule({
@@ -16,7 +17,7 @@ import {HttpModule} from '@angular/http';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
